1e5597482d39bb84a89312499f5447cd787633a7
Igor Soloshenko <siamajor@ukr.net>
2021-05-14 09:16:39 +0300

    Add files via upload

--- uninstall.php
+++ uninstall.php
@@ -6,8 +6,8 @@ if (!defined('WP_UNINSTALL_PLUGIN'))
 	exit;
 
 	delete_option('saints_option');
-	$dir = $_SERVER['DOCUMENT_ROOT'] . '/wp-content/uploads/saints-cache/';
+	$dir = wp_get_upload_dir()['basedir'] . '/saints-cache/';
 
-	require_once ( ABSPATH . '\wp-admin\includes\class-wp-filesystem-direct.php' );
+	require_once ( ABSPATH . '/wp-admin/includes/class-wp-filesystem-direct.php' );
 $fileSystemDirect = new WP_Filesystem_Direct(false);
 $fileSystemDirect->rmdir($dir, true);
